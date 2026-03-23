2176138ae86dcf7a1a4a115ddf0c9ba6c75c3465
UBSITAlcedoEllyn <20242248@s.ubaguio.edu>
2026-03-23 09:50:35 +0800

    More fixes on the code and now all routings and components are working as expected

--- TaskFlow/src/app/comps/task-edit/task-edit.component.ts
+++ TaskFlow/src/app/comps/task-edit/task-edit.component.ts
@@ -1,24 +1,35 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { TaskService } from '../../services/task.service';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-task-edit',
   standalone: true,
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './task-edit.component.html'
 })
 
 export class TaskEditComponent {
-  task: any;
-  constructor(private route: ActivatedRoute, private service: TaskService, private router: Router) {
-    const id = Number(this.route.snapshot.parent?.paramMap.get('id'));
-    this.task = { ...this.service.getTask(id) };
-}
+  task: Task | null = null;
 
-save() {
-  this.service.updateTask(this.task);
-  this.router.navigate(['/']);
-}
+  constructor(
+    private route: ActivatedRoute,
+    private service: TaskService,
+    private router: Router
+  ) {
+    const id = Number(this.route.snapshot.parent?.paramMap.get('id'));
+    const found = this.service.getTask(id);
+    this.task = found ? { ...found } : null;
+  }
+
+  save() {
+    if (!this.task) {
+      return;
+    }
+    this.service.updateTask(this.task);
+    this.router.navigate(['/']);
+  }
 }
