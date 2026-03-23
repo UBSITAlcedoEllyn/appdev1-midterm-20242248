3c63c9d894d6df2cb01618078c3050c15ea3e877
UBSITAlcedoEllyn <20242248@s.ubaguio.edu>
2026-03-23 09:26:38 +0800

    Added the missing template url for task-info

--- TaskFlow/src/app/comps/task-info/task-info.component.ts
+++ TaskFlow/src/app/comps/task-info/task-info.component.ts
@@ -3,15 +3,15 @@ import { ActivatedRoute } from '@angular/router';
 import { TaskService } from '../../services/task.service';
 
 @Component({
-selector: 'app-task-info',
-standalone: true,
-
+  selector: 'app-task-info',
+  standalone: true,
+  templateUrl: './task-info.component.html'
 })
-export class TaskInfoComponent {
-task: any;
 
-constructor(private route: ActivatedRoute, private service: TaskService) {
-const id = Number(this.route.snapshot.parent?.paramMap.get('id'));
-this.task = this.service.getTask(id);
-}
+export class TaskInfoComponent {
+  task: any;
+  constructor(private route: ActivatedRoute, private service: TaskService) {
+    const id = Number(this.route.snapshot.parent?.paramMap.get('id'));
+    this.task = this.service.getTask(id);
+  }
 }
