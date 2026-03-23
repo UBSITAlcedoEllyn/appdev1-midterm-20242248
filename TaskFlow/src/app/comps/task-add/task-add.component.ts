1891ff5aacaadfc1ebb23fdd252bcf12326f0c84
UBSITAlcedoEllyn <20242248@s.ubaguio.edu>
2026-03-23 09:17:31 +0800

    Added codes for the task-add with input option such as title, description, due date and the status and priority level

--- TaskFlow/src/app/comps/task-add/task-add.component.ts
+++ TaskFlow/src/app/comps/task-add/task-add.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TaskService } from '../../services/task.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-task-add',
-  imports: [],
-  templateUrl: './task-add.component.html',
-  styleUrl: './task-add.component.css'
+selector: 'app-task-add',
+standalone: true,
+imports: [FormsModule],
+templateUrl: './task-add.component.html'
+
 })
 export class TaskAddComponent {
+task: any = { status: 'Pending', priority: 'Low' };
 
+constructor(private service: TaskService, private router: Router) {}
+
+submit() {
+this.service.addTask(this.task);
+this.router.navigate(['/']);
+}
 }
