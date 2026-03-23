60ad69082eae4480fcd2bf640198a3dc738206c9
UBSITAlcedoEllyn <20242248@s.ubaguio.edu>
2026-03-23 09:10:17 +0800

    Added codes to task-list that will implement the required methods

--- TaskFlow/src/app/comps/task-list/task-list.component.ts
+++ TaskFlow/src/app/comps/task-list/task-list.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TaskService } from '../../services/task.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
-  templateUrl: './task-list.component.html',
-  styleUrl: './task-list.component.css'
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './task-list.component.html'
 })
 export class TaskListComponent {
+  tasks: any[] = [];
 
+  constructor(private service: TaskService) {
+    this.tasks = this.service.getTasks();
+  }
+
+  delete(id: number) {
+    this.service.deleteTask(id);
+    this.tasks = this.service.getTasks();
+  }
 }
