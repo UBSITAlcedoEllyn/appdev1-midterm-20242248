03e472abcf668911b15b408c97f32e663dcec55a
UBSITAlcedoEllyn <20242248@s.ubaguio.edu>
2026-03-23 09:44:14 +0800

    did some fixes

--- TaskFlow/src/app/app.routes.ts
+++ TaskFlow/src/app/app.routes.ts
@@ -6,8 +6,7 @@ import { TaskAddComponent } from './comps/task-add/task-add.component';
 export const routes: Routes = [
     { path: '', component: TaskListComponent },
     { path: 'add', component: TaskAddComponent },
-    {path: 'task/:id',component: TaskDetailComponent,
-        
+    { path: 'task/:id',component: TaskDetailComponent,
         children: [
             { path: '', loadComponent: () => import('./comps/task-info/task-info.component').then(m => m.TaskInfoComponent) },
             { path: 'edit', loadComponent: () => import('./comps/task-edit/task-edit.component').then(m => m.TaskEditComponent) }
--- TaskFlow/src/app/comps/task-detail/task-detail.component.ts
+++ TaskFlow/src/app/comps/task-detail/task-detail.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet, ActivatedRoute, RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { TaskService } from '../../services/task.service';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-task-detail',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet],
   templateUrl: './task-detail.component.html'
 })
 
-export class TaskDetailComponent {}
+export class TaskDetailComponent implements OnInit {
+  id = 0;
+  task: Task | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private service: TaskService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.id = Number(params.get('id'));
+      this.task = this.service.getTask(this.id);
+    });
+  }
+}
